c8af7fb64dde758a4eaaa849be66b982f233eb4a
yoon-H <71068767+yoon-H@users.noreply.github.com>
2022-11-20 02:57:24 +0900

    한윤재 로그인 구현
    한윤재 로그인 구현

--- app/src/main/java/com/example/stphotozone/ChallengeActivity.java
+++ app/src/main/java/com/example/stphotozone/ChallengeActivity.java
@@ -1,15 +1,34 @@
 package com.example.stphotozone;
 
+import static android.content.ContentValues.TAG;
+
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class ChallengeActivity extends AppCompatActivity {
 
@@ -21,24 +40,36 @@ public class ChallengeActivity extends AppCompatActivity {
     RadioGroup missionGroup;
     ImageButton imgBtnBack;
 
+    private FirebaseAuth firebaseAuth;
+    FirebaseFirestore db;
+
+    TextView nickName;
+    TextView missionCount;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.challenge);
 
-        missionGroup = findViewById(R.id.rgMission);
-        imgBtnBack = findViewById(R.id.imgBtnBack);
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        mission_grid = findViewById(R.id.grid_mission);
+        missionGroup = (RadioGroup) findViewById(R.id.rgMission);
+        imgBtnBack = (ImageButton) findViewById(R.id.imgBtnBack);
+
+        nickName =  (TextView) findViewById(R.id.txtNickname);
+        missionCount = (TextView) findViewById(R.id.txtCount);
+
+        setUserInfo();
+
+        mission_grid = (GridView) findViewById(R.id.grid_mission);
         mission_gridAdt = new GridAdapter(this);
 
-        System.out.println("after Adapter");
         mission_gridAdt.setItem( getString(R.string.BD_name), getString(R.string.BD_description), Character.BlackDragon);
         mission_gridAdt.setItem( getString(R.string.Tech_name), getString(R.string.Tech_description), Character.Tech);
         mission_gridAdt.setItem( getString(R.string.Ahyu_name), getString(R.string.Ahyu_description), Character.Ahyu);
 
-        System.out.println("after setItem");
 
         for(int j=0; j <mission_gridAdt.getArrayCount(); j++)
         {
@@ -123,5 +154,36 @@ public class ChallengeActivity extends AppCompatActivity {
         });
 
         System.out.println("oncreate");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setUserInfo() {
+        CollectionReference productRef = db.collection("users");
+        productRef
+                .whereEqualTo("email", firebaseAuth.getCurrentUser().getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+                            QuerySnapshot document =  task.getResult();
+                            Map<String, Object> data = document.getDocuments().get(0).getData();
+
+                            String name = data.get("nickname").toString();
+                            String num = data.get("mission_num").toString();
+
+                            nickName.setText(name);
+                            missionCount.setText(num);
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/stphotozone/MainActivity.java
+++ app/src/main/java/com/example/stphotozone/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.stphotozone;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -43,8 +46,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(getApplicationContext(), ChallengeActivity.class);
-                someActivityResultLauncher.launch(intent);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+                Log.d(TAG, "This is MainActivity");
 
             }
         });
