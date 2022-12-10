bb4a2e69c1e07539a3f3d0d3d1d518ba7d7409cd
ddubi <juijeong8324@gmail.com>
2022-12-10 20:52:18 +0900

    전의정_영상촬영 기능 추가

--- app/src/main/java/com/example/stphotozone/CameraActivity.java
+++ app/src/main/java/com/example/stphotozone/CameraActivity.java
@@ -2,56 +2,62 @@ package com.example.stphotozone;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentOnAttachListener;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.media.CamcorderProfile;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.helper.FirebaseManager;
-import com.example.helper.ResolveDialogFragment;
+import com.example.helper.VideoRecorder;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.Config;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
 import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.ArSceneView;
-import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.Sceneform;
-import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.rendering.Renderable;
-import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
 
 public class CameraActivity extends AppCompatActivity implements
         FragmentOnAttachListener,
-        BaseArFragment.OnSessionConfigurationListener {
+        BaseArFragment.OnSessionConfigurationListener{
+
+    // Video Recorder
+    private VideoRecorder videoRecorder;
+    public Camera c;
 
     // cloud 앵커를 위함
     FirebaseManager firebaseManager = new FirebaseManager(); // firebase에 클라우드 앵커 저장하기
@@ -59,32 +65,29 @@ public class CameraActivity extends AppCompatActivity implements
     FirebaseStorage storage;
     StorageReference model1, model2, model3;
     ArrayList<File> file = new ArrayList<File>(); // 파일 위치 저장
+    SurfaceTexture surfaceTexture;
 
     // IconButton
     ImageButton gallery, map, challenge, more, take_photo;
 
     // object of ArFragment Class
-    private ArFragment arCam;
+    private ArFragment arFragment;
     Anchor cloudAnchor;
-
-    private int clickNo = 0;
 
     // check the phone' hardware
     // checking whether the API version of the running Android >=24
-    public static boolean checkCameraSystem(Activity activity){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+    public static boolean checkCameraSystem(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             String openGlVersion = ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE))).getDeviceConfigurationInfo().getGlEsVersion();
-
             // Checking whether the OpenGL version >= 3.0
-            if(Double.parseDouble(openGlVersion) >= 3.0){
+            if (Double.parseDouble(openGlVersion) >= 3.0) {
                 return true;
-            }
-            else{
+            } else {
                 Toast.makeText(activity, "App needs OpenGl Version 3.0 or later", Toast.LENGTH_SHORT).show();
                 activity.finish();
                 return false;
             }
-        }else {
+        } else {
             Toast.makeText(activity, "App does not support required Build Version", Toast.LENGTH_SHORT).show();
             activity.finish();
             return false;
@@ -95,10 +98,36 @@ public class CameraActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        // 카메라 촬영을 위한..
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+            arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arArea);
+        }
+
+        //  ar코어를 사용하기 위한 hardware check
+        if (checkCameraSystem(this)) {
+
+            getSupportFragmentManager().addFragmentOnAttachListener(this);
+            //ArFragment is linked up with its respective id used in the activity_main.xml
+
+            if (savedInstanceState == null) {
+                if (Sceneform.isSupported(this)) {
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.arArea, ArFragment.class, null)
+                            .commit();
+                }
+            }
+
+        } else { // checkCameraSystem false
+            return;
+        }
+
         // Firebase를 위함
         FirebaseApp.initializeApp(this);
         storage = FirebaseStorage.getInstance();
-
 
         // 모델 관련
         // 모델 파일 불러옥
@@ -124,7 +153,6 @@ public class CameraActivity extends AppCompatActivity implements
             e.printStackTrace();
         }
 
-
         /*초기화*/
         take_photo = (ImageButton) findViewById(R.id.take_photo);
         gallery = (ImageButton) findViewById(R.id.gallery);
@@ -132,33 +160,17 @@ public class CameraActivity extends AppCompatActivity implements
         challenge = (ImageButton) findViewById(R.id.challenge);
         more = (ImageButton) findViewById(R.id.more);
 
-        // hardware check
-        if(checkCameraSystem(this)){
 
-            getSupportFragmentManager().addFragmentOnAttachListener(this);
-            //ArFragment is linked up with its respective id used in the activity_main.xml
-
-            if (savedInstanceState == null) {
-                if (Sceneform.isSupported(this)) {
-                    getSupportFragmentManager().beginTransaction()
-                            .add(R.id.arArea, ArFragment.class, null)
-                            .commit();
-                }
-            }
-
-        } else { // checkCameraSystem false
-            return;
-        }
 
         /* 버튼 클릭 처리 부분*/
-        map.setOnClickListener(new View.OnClickListener() {
+        map.setOnClickListener(new View.OnClickListener() { // 지도 액티비티
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MapActivity.class);
                 startActivity(intent);
             }
         });
-        challenge.setOnClickListener(new View.OnClickListener() {
+        challenge.setOnClickListener(new View.OnClickListener() { // 도전과제 액티비티
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class); // 먼저 로그인이 되어야 함
@@ -166,39 +178,88 @@ public class CameraActivity extends AppCompatActivity implements
             }
         });
 
-        // 찾기
-        take_photo.setOnClickListener(view -> {
-            // 짧은 코드로 reolve할 모델 찾기!!
-            firebaseManager.getCloudAnchorId(147, cloudAnchorId -> { // firebase에서 찾아보세용~
-                if(cloudAnchorId == null || cloudAnchorId.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "A Cloud Anchor ID for the short code " + 147 + " was not found.", Toast.LENGTH_SHORT).show(); // 찾을 수 없다!!
-                    return;
+        more.setOnClickListener(new View.OnClickListener() { // 더보기 액티비티
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CloudActivity.class); // 클라우드 앵커 만들기
+                startActivity(intent);
+            }
+        });
+
+        gallery.setOnClickListener(new View.OnClickListener() { // 갤러리 액티비티
+            @Override
+            public void onClick(View view) {
+                // 이 부분 다름!!!!!!!!!!!
+
+//                Intent intent = new Intent(Intent.ACTION_PICK); // 갤러리에 접근하도록 선택하게 설정
+//                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+//                intent.setType("image/*"); // 이미지 저장하는 애들만 열도록!!!
+//                startActivity(intent);
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivity(intent);
+            }
+        });
+
+
+        // 사진 촬영
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 모델 불러오기
+                // 짧은 코드로 reolve할 모델 찾기!!
+                firebaseManager.getCloudAnchorId(148, cloudAnchorId -> { // firebase에서 찾아보세용~
+                    if(cloudAnchorId == null || cloudAnchorId.isEmpty()){
+                        Toast.makeText(getApplicationContext(), "A Cloud Anchor ID for the short code " + 147 + " was not found.", Toast.LENGTH_SHORT).show(); // 찾을 수 없다!!
+                        return;
+                    }
+
+                    cloudAnchor = arFragment.getArSceneView().getSession().resolveCloudAnchor(cloudAnchorId); // 해당 위치로 Anchor 가져오기
+
+                }, model -> {
+                    Log.d("fuu2", cloudAnchor+"");
+                    createModel(cloudAnchor, model); // 모델 만들어!
+                    Toast.makeText(getApplicationContext(), "Now resolving anchor...", Toast.LENGTH_SHORT).show();
+                });
+
+                // 영상 test
+                if(videoRecorder == null){
+                    videoRecorder = new VideoRecorder();
+                    videoRecorder.setSceneView(arFragment.getArSceneView());
+
+                    int orientation = getResources().getConfiguration().orientation;
+
+                    videoRecorder.setVideoQuality(CamcorderProfile.QUALITY_HIGH, orientation);
                 }
 
-                cloudAnchor = arCam.getArSceneView().getSession().resolveCloudAnchor(cloudAnchorId); // 해당 위치로 Anchor 가져오기
+                boolean isRecording  = videoRecorder.onToggleRecord(); // 녹화 시작
 
-            }, model -> {
-                Log.d("fuu2", cloudAnchor+"");
-                createModel(cloudAnchor, model); // 모델 만들어!
-            });
+                if(isRecording)
+                    Toast.makeText(getApplicationContext(), "Start Recording", Toast.LENGTH_SHORT).show();
+                else{
+                    Toast.makeText(getApplicationContext(), "Recording stopped", Toast.LENGTH_SHORT).show();
+                }
 
-            Toast.makeText(getApplicationContext(), "Now resolving anchor...", Toast.LENGTH_SHORT).show();
+            }
+
         });
+
     }
 
     @Override
     public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
         // ArFragment에 각종 설정 해주기
-        if(fragment.getId() == R.id.arArea){
-            arCam = (ArFragment) fragment;
-            arCam.setOnSessionConfigurationListener(this);
+        if (fragment.getId() == R.id.arArea) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnSessionConfigurationListener(this); // 여기서 set!!
             //arCam.setOnTapArPlaneListener(this);
         }
 
     }
 
     @Override
-    public void onSessionConfiguration(Session session, Config config) {
+    public void onSessionConfiguration(Session session, Config config) { // AR코어 세션에 설정
         if(session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)){ // Depth 모드 지원되니?!
             config.setDepthMode(Config.DepthMode.AUTOMATIC);
         }
@@ -206,7 +267,7 @@ public class CameraActivity extends AppCompatActivity implements
     }
 
     // 모델 렌더링
-    private void createModel(Anchor anchor, int model){
+    private void createModel(Anchor anchor, int model) {
         ModelRenderable.builder()
                 .setSource(this, Uri.parse(file.get(model).getPath()))
                 .setIsFilamentGltf(true)
@@ -223,18 +284,19 @@ public class CameraActivity extends AppCompatActivity implements
     }
 
     // 모델 앵커로 위치시키기
-    private void placeModel(Anchor anchor, ModelRenderable modelRenderable){
+    private void placeModel(Anchor anchor, ModelRenderable modelRenderable) {
         // Create the Anchor.
         AnchorNode anchorNode = new AnchorNode(anchor);
-        anchorNode.setParent(arCam.getArSceneView().getScene());
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
 
         // Create the transformable model and add it to the anchor.
-        TransformableNode model = new TransformableNode(arCam.getTransformationSystem());
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
         model.setParent(anchorNode);
         model.setRenderable(modelRenderable)
                 .animate(true).start();
-        arCam.getArSceneView().getScene().addChild(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
         model.select();
 
     }
+
 }
