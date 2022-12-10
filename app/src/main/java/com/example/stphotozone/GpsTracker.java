5773fd20c2caa03b325726122937f0da6a5fbd36
yoon-H <71068767+yoon-H@users.noreply.github.com>
2022-12-10 16:12:11 +0900

    한윤재 지도 갱신 기능
    한윤재 지도 갱신 기능

--- app/src/main/java/com/example/stphotozone/GpsTracker.java
+++ app/src/main/java/com/example/stphotozone/GpsTracker.java
@@ -19,86 +19,134 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
 
 public class GpsTracker extends Service implements LocationListener {
 
     private final Context mContext;
     Location location;
 
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 5 * 1;
     protected LocationManager locationManager;
 
     //LocationListener 정보
-    String provider;
     double longitude;
     double latitude;
-    double altitude;
 
 
     public GpsTracker(Activity activity, Context context) {
         this.mContext = context;
-        locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
-        getlocation(activity);
-    }
 
-    @SuppressLint("MissingPermission")
-    public void getlocation(Activity _activity) {
-        if ( Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission( mContext, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( _activity , new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    0 );
-        }
-        else{
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            provider = location.getProvider();
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-            altitude = location.getAltitude();
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    1000,
-                    1,
-                    gpsLocationListener);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                    1000,
-                    1,
-                    gpsLocationListener);
-        }
+        getLocation(activity);
     }
 
 
-    final LocationListener gpsLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(@NonNull Location location) {
-            provider = location.getProvider();
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-            altitude = location.getAltitude();
+
+    public Location getLocation(Activity _activity) {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+
+            } else {
+
+                int hasFineLocationPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION);
+                int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+                if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                        hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+                    if (isNetworkEnabled) {
+
+                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            Log.d("Tracker1 : ", location.toString());
+
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                Log.d("Tracker1, latitude : ", String.valueOf(latitude));
+                                longitude = location.getLongitude();
+                                Log.d("Tracker1, longitude : ", String.valueOf(longitude));
+                            }
+                        }
+                    }
+
+                    if(isGPSEnabled)
+                    {
+                        if (location ==null)
+                        {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+                            if(locationManager != null)
+                            {
+                                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                Log.d("Tracker2 : ", location.toString());
+                                if (location != null)
+                                {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    return null;
+                }
+
+
+
+            }
+        }
+        catch (Exception e)
+        {
+            Log.d("@@@", ""+e.toString());
         }
 
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            LocationListener.super.onStatusChanged(provider, status, extras);
-        }
+        return location;
+    }
 
-        @Override
-        public void onProviderEnabled(@NonNull String provider) {
-            LocationListener.super.onProviderEnabled(provider);
-        }
 
-        @Override
-        public void onProviderDisabled(@NonNull String provider) {
-            LocationListener.super.onProviderDisabled(provider);
-        }
-    };
+//    final LocationListener gpsLocationListener = new LocationListener() {
+//        @Override
+//        public void onLocationChanged(@NonNull Location location) {
+//            provider = location.getProvider();
+//            longitude = location.getLongitude();
+//            latitude = location.getLatitude();
+//            altitude = location.getAltitude();
+//        }
+//
+//
+//    };
 
-    @Nullable
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 
     @Override
@@ -107,11 +155,27 @@ public class GpsTracker extends Service implements LocationListener {
     }
 
     public double getLongitude() {
+        if(location != null)
+        {
+            longitude = location.getLongitude();
+        }
         return longitude;
     }
 
     public double getLatitude() {
+        if(location != null)
+        {
+            latitude = location.getLatitude();
+        }
         return latitude;
+    }
+
+    public void stopUsingGPS()
+    {
+        if(locationManager != null)
+        {
+            locationManager.removeUpdates(GpsTracker.this);
+        }
     }
 
 
